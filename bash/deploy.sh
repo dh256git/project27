e10b254414fb789810ee84677129ac83d04355be
Daniel Hajas <d.hajas@icloud.com>
2021-03-08 11:32:34 +0000

    Implemented script to automatically increment commit count during deployment.

--- bash/deploy.sh
+++ bash/deploy.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 # script to automate git add, commit, and push
+
+## Go to project directory
 cd ~/Documents/self/informatics/projects/project27/src/
+
+## execute version-counter python script to increment commit count
+python3 bash/version-counter.py
+
+## initiate git process
 echo "Add commit message"
 read commitMessage
 git add .
