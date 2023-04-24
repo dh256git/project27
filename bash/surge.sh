572e9b98804cccc160bd9d9a020f890a2819687d
Daniel Hajas <d.hajas@icloud.com>
2023-04-24 21:26:47 +0100

    Writing a surge script and editing announcements to be conditional.

---
+++ bash/surge.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# script to semi-automate deployment of static test site files onto Surge.
+
+## Go to project root directory
+cd ~/Documents/self/informatics/projects/project27/
+
+## execute version-counter python script to increment commit count
+# python3 bash/version-counter.py # commenting out for surge deployment
+
+## build site from source using config default baseurl
+cd src/
+JEKYLL_ENV=production jekyll build
+
+## Pause for 3 seconds
+sleep 3
+
+## copy site files to surge directory
+cp -r _site/* ../surge/
+
+## initiate git push to staging branch
+# git add _layouts/default.html # comment out as version counter is not incremented.
+cd ../surge/
+git add . # add all docs files to deployed content.
+echo "Add commit message"
+read commitMessage
+git commit -m "$commitMessage"
+git push origin staging
+echo "Unstable release is ready to surge."
--- src/support/index.md
+++ src/support/index.md
@@ -6,6 +6,7 @@ author: Daniel Hajas
 reviewer: Danielle Garratt
 date: 2022-10-15
 licence: copyright
+announcement: Currently, there are no available sessions to book.
 buttonStyle: fg-general-vol
 backgroundStyle: bg-general-vol
 ---
